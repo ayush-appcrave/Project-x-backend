bfe40921925208d5e5b36837769202c24e590132
ayush-appcrave <ayush.talesara777.appcrave@gmail.com>
2025-01-22 15:47:20 +0530

    feat(middleware): add request tracker middleware

--- src/middlewares/requestTracer.middleware.js
+++ src/middlewares/requestTracer.middleware.js
@@ -2,11 +2,37 @@ const requestTracer = (req, res, next) => {
     const startTime = Date.now()
     const requestId = Math.random().toString(36).substring(7)
     
-    console.log(`[${requestId}] ${req.method} ${req.url} - Started`)
-    
+    // Color codes
+    const colors = {
+        reset: '\x1b[0m',
+        bright: '\x1b[1m',
+        cyan: '\x1b[36m',
+        green: '\x1b[32m',
+        yellow: '\x1b[33m',
+        blue: '\x1b[34m',
+        magenta: '\x1b[35m'
+    }
+
+    // Request start log
+    console.log(
+        `${colors.bright}${colors.cyan}[${requestId}]${colors.reset} ` +
+        `${colors.yellow}${req.method}${colors.reset} ` +
+        `${colors.blue}${req.url}${colors.reset} ` +
+        `${colors.magenta}Started${colors.reset}`
+    )
+
     res.on('finish', () => {
         const duration = Date.now() - startTime
-        console.log(`[${requestId}] ${req.method} ${req.url} - ${res.statusCode} - ${duration}ms`)
+        const statusColor = res.statusCode < 400 ? colors.green : '\x1b[31m' // Red for errors
+
+        // Request completion log
+        console.log(
+            `${colors.bright}${colors.cyan}[${requestId}]${colors.reset} ` +
+            `${colors.yellow}${req.method}${colors.reset} ` +
+            `${colors.blue}${req.url}${colors.reset} ` +
+            `${statusColor}${res.statusCode}${colors.reset} ` +
+            `${colors.magenta}${duration}ms${colors.reset}`
+        )
     })
 
     next()
